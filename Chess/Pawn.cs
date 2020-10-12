1656595f314166a6db73ed60ec3923d07befc079
Nathaniel <nathaniel.ollie.smith@gmail.com>
2020-10-12 23:00:28 +0100

    fixed bug with white pawn not taking first double move

--- Chess/Pawn.cs
+++ Chess/Pawn.cs
@@ -26,17 +26,17 @@ namespace Chess
             if (point.y > 0 && point.y < 8 && board.GetPiece(point.x, point.y + (GetColour() ? -1 : 1)) == null)
             {
                 moves.Add(new Vector(point, new Point(point.x, point.y + (GetColour() ? -1 : 1))));
-                if (point.y == 1 || point.y == 7)
+                if (point.y == 1 || point.y == 6)
                 {
                     moves.Add(new Vector(point, new Point(point.x, point.y + (GetColour() ? -2 : 2))));
                 }
             }
             List<Vector> attacks = new List<Vector>();
-            if (point.y > 0 && point.y < 8 && point.x < 7 && board.GetPiece(point.x + 1, point.y + (GetColour() ? -1 : 1)) != null)
+            if (point.y > 0 && point.y < 8 && point.x < 7 && board.GetPiece(point.x + 1, point.y + (GetColour() ? -1 : 1)) is Piece p1 && p1.GetColour() != GetColour())
             {
                 attacks.Add(new Vector(point, new Point(point.x + 1, point.y + (GetColour() ? -1 : 1))));
             }
-            if (point.y > 0 && point.y < 8 && point.x > 1 && board.GetPiece(point.x - 1, point.y + (GetColour() ? -1 : 1)) != null)
+            if (point.y > 0 && point.y < 8 && point.x > 1 && board.GetPiece(point.x - 1, point.y + (GetColour() ? -1 : 1)) is Piece p2 && p2.GetColour() != GetColour())
             {
                 attacks.Add(new Vector(point, new Point(point.x - 1, point.y + (GetColour() ? -1 : 1))));
             }
