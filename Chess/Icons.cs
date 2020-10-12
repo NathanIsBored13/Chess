cecac77c4bbca4feeda1008ac57299b5c4cf8d8f
Nathaniel <nathaniel.ollie.smith@gmail.com>
2020-10-12 22:54:30 +0100

    fixed icon ID mismatch

--- Chess/Icons.cs
+++ Chess/Icons.cs
@@ -16,15 +16,15 @@ namespace Chess
         {
             images[0] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WKing.png"));
             images[1] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WQueen.png"));
-            images[2] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WBishop.png"));
-            images[3] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WRook.png"));
-            images[4] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WKnight.png"));
+            images[2] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WRook.png"));
+            images[3] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WKnight.png"));
+            images[4] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WBishop.png"));
             images[5] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\WPawn.png"));
             images[6] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BKing.png"));
             images[7] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BQueen.png"));
-            images[8] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BBishop.png"));
-            images[9] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BRook.png"));
-            images[10] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BKnight.png"));
+            images[8] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BRook.png"));
+            images[9] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BKnight.png"));
+            images[10] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BBishop.png"));
             images[11] = new BitmapImage(new Uri($@"{Environment.CurrentDirectory}\Resources\{folderName}\BPawn.png"));
 
             foreach (Action action in listeneres)
