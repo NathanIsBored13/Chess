24350bcfe4de708332d3f52b1b8ec2a761e84ad0
Nathaniel <nathaniel.ollie.smith@gmail.com>
2020-12-15 22:05:05 +0000

    simplified pawn code

--- Chess/Pawn.cs
+++ Chess/Pawn.cs
@@ -10,9 +10,11 @@ namespace Chess
 {
     class Pawn : Piece
     {
+        private readonly int dir;
+
         public Pawn(bool colour, Point position) : base(colour, position)
         {
-
+            dir = colour ? -1 : 1;
         }
 
         public override Type GetType()
@@ -23,22 +25,22 @@ namespace Chess
         public override PieceMovesMask GetMovesMask(Board board)
         {
             BitBoard moves = new BitBoard();
-            if (GetPosition().y > 0 && GetPosition().y < 8 && board.GetPiece(GetPosition().x, GetPosition().y + (GetColour() ? -1 : 1)) == null)
+            if (GetPosition().y > 0 && GetPosition().y < 8 && board.GetPiece(GetPosition().x, GetPosition().y + dir) == null)
             {
-                moves.Set(new Point(GetPosition().x, GetPosition().y + (GetColour() ? -1 : 1)));
-                if (GetPosition().y == 1 || GetPosition().y == 6 && board.GetPiece(GetPosition().x, GetPosition().y + (GetColour() ? -2 : 2)) == null)
+                moves.Set(new Point(GetPosition().x, GetPosition().y + dir));
+                if (GetPosition().y == 1 || GetPosition().y == 6 && board.GetPiece(GetPosition().x, GetPosition().y + 2 * dir) == null)
                 {
-                    moves.Set(new Point(GetPosition().x, GetPosition().y + (GetColour() ? -2 : 2)));
+                    moves.Set(new Point(GetPosition().x, GetPosition().y + 2 * dir));
                 }
             }
             BitBoard attacks = new BitBoard();
-            if (GetPosition().y > 0 && GetPosition().y < 8 && GetPosition().x < 7 && board.GetPiece(GetPosition().x + 1, GetPosition().y + (GetColour() ? -1 : 1)) is Piece p1 && p1.GetColour() != GetColour())
+            if (GetPosition().y > 0 && GetPosition().y < 8 && GetPosition().x < 7 && board.GetPiece(GetPosition().x + 1, GetPosition().y + dir) is Piece p1 && p1.GetColour() != GetColour())
             {
-                attacks.Set(new Point(GetPosition().x + 1, GetPosition().y + (GetColour() ? -1 : 1)));
+                attacks.Set(new Point(GetPosition().x + 1, GetPosition().y + dir));
             }
-            if (GetPosition().y > 0 && GetPosition().y < 8 && GetPosition().x > 1 && board.GetPiece(GetPosition().x - 1, GetPosition().y + (GetColour() ? -1 : 1)) is Piece p2 && p2.GetColour() != GetColour())
+            if (GetPosition().y > 0 && GetPosition().y < 8 && GetPosition().x > 1 && board.GetPiece(GetPosition().x - 1, GetPosition().y + dir) is Piece p2 && p2.GetColour() != GetColour())
             {
-                attacks.Set(new Point(GetPosition().x - 1, GetPosition().y + (GetColour() ? -1 : 1)));
+                attacks.Set(new Point(GetPosition().x - 1, GetPosition().y + dir));
             }
             return new PieceMovesMask(attacks, moves);
         }
@@ -47,9 +49,9 @@ namespace Chess
         {
             BitBoard seen = new BitBoard();
             if (GetColour() ? (GetPosition().y > 0) : (GetPosition().y < 7) && GetPosition().y < 7 && GetPosition().x > 0)
-                seen.Set(new Point(GetPosition().x - 1, GetPosition().y + (GetColour() ? -1 : 1)));
+                seen.Set(new Point(GetPosition().x - 1, GetPosition().y + dir));
             if (GetColour() ? (GetPosition().y > 0) : (GetPosition().y < 7) && GetPosition().y > 0 && GetPosition().x > 0)
-                seen.Set(new Point(GetPosition().x + 1, GetPosition().y + (GetColour() ? -1 : 1)));
+                seen.Set(new Point(GetPosition().x + 1, GetPosition().y + dir));
             return seen;
         }
     }
